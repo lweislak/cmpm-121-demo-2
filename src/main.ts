3e4220a4380fb8b2741597fa013c71d737e93fc3
lweislak <150990328+lweislak@users.noreply.github.com>
2024-10-20 02:22:20 -0700

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,11 @@ const APP_NAME = "Hello!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+//app.innerHTML = APP_NAME;
+
+const appTitle = document.createElement("h1");
+
+const canvas: HTMLCanvasElement = document.createElement('canvas');
+const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!; //Check that result isn't null
+
+app.append(canvas);
